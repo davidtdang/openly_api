c2c550407df1fdf415bc6851d43130613e30d5f9
David Dang <dtudang@gmail.com>
2015-03-31 23:48:09 -0700

    stable yelp after rounded search front end commit

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,60 +1,10 @@
+
 class VenuesController < ApplicationController
   respond_to :json
 
-  def splash
-
-  end
-
-  def show
-
-    # response = Yelp.client.search('San Francisco', { term: params[:search], coordinates: "#{params[:lat]},#{params[:lng]}" })
-    # businesses = response.businesses
-    #
-    # # @geojson = Array.new
-    # # @businesses.each do |business|
-    # #   @geojson << {
-    # #       type: 'Feature',
-    # #       geometry: {
-    # #           type: 'Point',
-    # #           coordinates: [business.location.coordinate.longitude, business.location.coordinate.latitude]
-    # #       },
-    # #       properties: {
-    # #           name: business.name,
-    # #           address: "#{business.location.display_address.first }, #{business.location.display_address.last}",
-    # #           :'marker-color' => '#00607d',
-    # #           :'marker-symbol' => 'circle',
-    # #           :'marker-size' => 'medium'
-    # #       }
-    # #   }
-    # #
-    # #   respond_to do |format|
-    # #     format.html
-    # #     format.json { render json: @geojson }
-    # #   end
-    # #
-    # # end
-    #
-    #
-    #
-    # @businesses = businesses.map do |business|
-    #   {
-    #     name: business.name,
-    #     url: business.url,
-    #     image_url: business.image_url,
-    #     review_count: business.review_count,
-    #     categories: business.categories,
-    #     star_rating: business.rating_img_url,
-    #     rating: business.rating,
-    #     address: "#{business.location.display_address.first }, #{business.location.display_address.last}",
-    #     phone: display_phone(business.display_phone)
-    #     geocoords: [business.location.coordinate.longitude, business.location.coordinate.latitude],
-    #   }
-    # end
-    # render restaurant_path
-  end
 
   def search_yelp
-    # response = Yelp.client.search('San Francisco', { term: params[:s], coordinates: "#{params[:lat]},#{params[:lng]}" })
+
     response = Yelp.client.search('San Francisco', {term: params[:s]})
 
 
@@ -67,7 +17,6 @@ class VenuesController < ApplicationController
       {
         name: business.name,
         url: business.url,
-        # image_url: business.image_url,
         review_count: business.review_count,
         categories: categories,
         star_rating: business.rating_img_url,
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   get 'venues/show' => 'venues#show', as: :venues
 
 
+
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
